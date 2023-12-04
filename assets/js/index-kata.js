95a298202a7c91b27bdf31d26866144eeb809afd
TzuruC <ostrich2821@gmail.com>
2023-12-05 03:44:51 +0800

    select on progress

--- assets/js/index-kata.js
+++ assets/js/index-kata.js
@@ -19,26 +19,34 @@ function getProductList() {
     axios.get(`${api_url}/${api_path}/products`)
         .then(function (res) {
             productData = res.data.products;
-            let str = "";
-            productData.forEach((item) => {
-                str += `
-            <li class="productCard">
-            <h4 class="productType">${item.category}</h4>
-            <img
-            src="${item.images}"
-            alt=""
-            />
-            <a href="#" class="addCardBtn">加入購物車</a>
-            <h3>${item.title}</h3>
-            <del class="originPrice">NT$${new Intl.NumberFormat('en-IN').format(item.origin_price)}</del>
-            <p class="nowPrice">NT$${new Intl.NumberFormat('en-IN').format(item.price)}</p>
-            </li>
-            `;
-                productList.innerHTML = str;
-            });
+            renderProductList();
         })
         .catch(function (error) {
             console.log(error.response.data);
         })
 }
 // 記錄點︰30~50分鐘
+
+productSelect.addEventListener('change', (e) => {
+    console.log(e.target.value);
+});
+
+function renderProductList() {
+    let str = "";
+    productData.forEach((item) => {
+        str += `
+    <li class="productCard">
+    <h4 class="productType">${item.category}</h4>
+    <img
+    src="${item.images}"
+    alt=""
+    />
+    <a href="#" class="addCardBtn">加入購物車</a>
+    <h3>${item.title}</h3>
+    <del class="originPrice">NT$${new Intl.NumberFormat('en-IN').format(item.origin_price)}</del>
+    <p class="nowPrice">NT$${new Intl.NumberFormat('en-IN').format(item.price)}</p>
+    </li>
+    `;
+        productList.innerHTML = str;
+    });
+}
