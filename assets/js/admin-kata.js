7e3c8112d51c910e294383e1a07380879d08c519
TzuruC <ostrich2821@gmail.com>
2023-12-07 04:36:56 +0800

    kata 1 finished

--- assets/js/admin-kata.js
+++ assets/js/admin-kata.js
@@ -6,40 +6,90 @@ const orderList = document.querySelector(".js-orderList");
 let orderData = [];
 
 
-function init(){
+function init() {
     getOrderList();
 }
 init(); // 畫面初始化
 
-function renderC3(){
+function renderC3() {
     //物件資料蒐集
     let total = {};
-    orderData.forEach((item)=>{
-        item.products.forEach((productItem)=>{
-            if(total[productItem.category] == undefined){
-                total[productItem.category] = productItem.price*productItem.quantity;
-            }else{
-                total[productItem.category] += productItem.price*productItem.quantity;
+    orderData.forEach((item) => {
+        item.products.forEach((productItem) => {
+            if (total[productItem.category] == undefined) {
+                total[productItem.category] = productItem.price * productItem.quantity;
+            } else {
+                total[productItem.category] += productItem.price * productItem.quantity;
             }
         });
     });
     let categoryAry = Object.keys(total);
     let newData = [];
-    categoryAry.forEach((item)=>{
-        let ary= [];
+    categoryAry.forEach((item) => {
+        let ary = [];
         ary.push(item);
         ary.push(total[item]);
         newData.push(ary);
     });
 
-//  - C3 表格
-let chart = c3.generate({
-    bindto: '#chart', // HTML 元素綁定
-    data: {
-        type: "pie",
-        columns: newData,        
-    },
-});
+    //  - C3 表格
+    let chart = c3.generate({
+        bindto: '#chart', // HTML 元素綁定
+        data: {
+            type: "pie",
+            columns: newData,
+        },
+    });
+}
+
+function renderC3_lv2() {
+    // 資料蒐集
+    let obj = {};
+    orderData.forEach((item) => {
+        item.products.forEach((productItem) => {
+            if (total[productItem.title] == undefined) {
+                total[productItem.title] = productItem.price * productItem.quantity;
+            } else {
+                total[productItem.title] += productItem.price * productItem.quantity;
+            }
+        });
+    });
+    //拉出資料關聯
+    let originAry = Object.keys(obj);
+
+    // 透過 originAry 整理出 C3 格式
+    let rankSortAry = [];
+    originAry.forEach((item) => {
+        let ary = [];
+        ary.push(item);
+        ary.push(obj[item]);
+        rankSortAry.push(ary);
+    });
+
+    // 用sort排序
+    rankSortAry.sort((a, b) => {
+        return b[1] - a[1]; // 比較對象是索引位置 [1] 也就是類別總額
+    });
+    // 超過 3 筆資料時，取出前 3 名
+    if (rankSortAry.length > 3) {
+        let otherTotal = 0;// 前四名以外的加總
+        rankSortAry.forEach((item, index) => {
+            if (index > 2) { //加總前 3 名 (索引 0,1,2 ) 以後的資料
+                otherTotal += rankSortAry[index][1];
+            }
+            rankSortAry.splice(3, rankSortAry.length - 1); // 去掉索引值 3 以後的項目
+            rankSortAry.push(['其他', otherTotal]); // 前四名以外的加總
+        });
+    }
+
+    // c3圖表
+    let chart = c3.generate({
+        bindto: '#chart', // HTML 元素綁定
+        data: {
+            type: "pie",
+            columns: rankSortAry,
+        },
+    });
 }
 
 // 取得訂單列表
@@ -55,15 +105,15 @@ function getOrderList() {
             orderData.forEach((item) => {
                 // 組產品字串
                 let productStr = "";
-                item.products.forEach((productItem)=>{
-                    productStr+=`<p>${productItem.title} × ${productItem.quantity}</p>`;
+                item.products.forEach((productItem) => {
+                    productStr += `<p>${productItem.title} × ${productItem.quantity}</p>`;
                 });
                 // 判斷訂單處理狀態
                 let orderStatus = "";
-                item.paid ? orderStatus="已處理" : orderStatus="未處理";
+                item.paid ? orderStatus = "已處理" : orderStatus = "未處理";
                 // 組訂單字串
 
-                str+=`<tr>
+                str += `<tr>
                 <td>${item.id}</td>
                 <td>
                     <p>${item.user.name}</p>
@@ -92,29 +142,29 @@ function getOrderList() {
 }
 
 // 未處理、刪除按鈕
-orderList.addEventListener('click', (e)=>{
+orderList.addEventListener('click', (e) => {
     e.preventDefault();
     const targetClass = e.target.getAttribute("class");
     let id = e.target.getAttribute("data-id");
 
     // 刪除按鈕
-    if(targetClass == "delSingleOrder-Btn js-orderDelete"){
+    if (targetClass == "delSingleOrder-Btn js-orderDelete") {
         deleteOrderItem(id);
         return;
     }
     // 訂單狀態按鈕
-    if(targetClass == "orderStatus"){
+    if (targetClass == "orderStatus") {
         let status = e.target.getAttribute("data-status")
-        changeOrderStatus(status,id);
+        changeOrderStatus(status, id);
         return;
     }
 });
 
-function changeOrderStatus(status,id){
+function changeOrderStatus(status, id) {
     let newStatus;
-    if(status==true){
+    if (status == true) {
         newStatus = false;
-    }else{
+    } else {
         newStatus = true;
     }
     axios.put(`${api_url}/${api_path}/orders`, {
@@ -127,22 +177,22 @@ function changeOrderStatus(status,id){
             "authorization": token
         }
     })
-    .then(function (res) {
-        alert("修改訂單狀態成功");
-        getOrderList();
-    })
+        .then(function (res) {
+            alert("修改訂單狀態成功");
+            getOrderList();
+        })
 }
 
-function deleteOrderItem(id){
+function deleteOrderItem(id) {
     axios.delete(`${api_url}/${api_path}/orders/${id}`, {
         headers: {
             "authorization": token
         }
     })
-    .then(function (res) {
-        alert("刪除成功");
-        getOrderList();
-    })
+        .then(function (res) {
+            alert("刪除成功");
+            getOrderList();
+        })
 }
 
 
@@ -163,15 +213,15 @@ function transDate(timeStamp) {
 
 
 const discardAllBtn = document.querySelector('.discardAllBtn');
-discardAllBtn.addEventListener('click',(e)=>{
+discardAllBtn.addEventListener('click', (e) => {
     e.preventDefault();
     axios.delete(`${api_url}/${api_path}/orders`, {
         headers: {
             "authorization": token
         }
     })
-    .then(function (res) {
-        alert("正在執行毀滅式刪除...");
-        getOrderList();
-    })
+        .then(function (res) {
+            alert("正在執行毀滅式刪除...");
+            getOrderList();
+        })
 });
--- assets/js/index-kata.js
+++ assets/js/index-kata.js
@@ -42,21 +42,21 @@ function renderProductList() {
 productSelect.addEventListener('change', (e) => {
     console.log(e.target.value);
     const category = e.target.value;
-    if(category == '全部'){
+    if (category == '全部') {
         renderProductList();
         return;
     };
     let str = "";
     productData.forEach((item) => {
-        if(item.category == category){
-            str += combineProductHTMLItem(item);            
+        if (item.category == category) {
+            str += combineProductHTMLItem(item);
         };
     });
     productList.innerHTML = str;
 });
 
 // 重構 product item HTML
-function combineProductHTMLItem(item){
+function combineProductHTMLItem(item) {
     return `
     <li class="productCard">
     <h4 class="productType">${item.category}</h4>
@@ -74,35 +74,35 @@ function combineProductHTMLItem(item){
 
 // 點擊加入購物車
 // js-addCart 改為 addCardBtn
-productList.addEventListener('click',(e)=>{
+productList.addEventListener('click', (e) => {
     e.preventDefault();
     let addCartClass = e.target.getAttribute("class");
-    if( addCartClass !== "addCardBtn"){
+    if (addCartClass !== "addCardBtn") {
         return;
     }
 
-    let productId = e.target.getAttribute('data-id');    
+    let productId = e.target.getAttribute('data-id');
     let numCheck = 1;
-    cartData.forEach((item)=>{
-        if(item.product.id === productId){
-            numCheck = item.quantity+=1;
+    cartData.forEach((item) => {
+        if (item.product.id === productId) {
+            numCheck = item.quantity += 1;
         };
     });
-    
+
     axios.post(`${api_url}/${api_path}/carts`, {
         "data": {
             "productId": productId,
             "quantity": numCheck
         }
     })
-    .then(function (res) {
-        const cartProductName = e.target.getAttribute('data-product');
-        alert(`"${cartProductName}" 已加入購物車`);
-        getCartList();
-    })
-    .catch(function (error) {
-        console.log(error.response.data);
-    })
+        .then(function (res) {
+            const cartProductName = e.target.getAttribute('data-product');
+            alert(`"${cartProductName}" 已加入購物車`);
+            getCartList();
+        })
+        .catch(function (error) {
+            console.log(error.response.data);
+        })
 });
 
 //取得購物車資料
@@ -112,7 +112,7 @@ function getCartList() {
             cartData = res.data.carts;
             document.querySelector('.js-total').textContent = new Intl.NumberFormat('en-IN').format(res.data.finalTotal);
             let str = "";
-            cartData.forEach((item)=>{
+            cartData.forEach((item) => {
                 str += `
                 <tr>
                     <td>
@@ -138,45 +138,45 @@ function getCartList() {
 }
 
 // 刪除特定筆購物車商品
-cartList.addEventListener('click',(e)=>{
+cartList.addEventListener('click', (e) => {
     e.preventDefault();
     const cartId = e.target.getAttribute('data-id');
     const cartProductName = e.target.getAttribute('data-product');
-    if(cartId == null){
+    if (cartId == null) {
         return;
     };
     axios.delete(`${api_url}/${api_path}/carts/${cartId}`)
-    .then(function (res) {
-        alert(`已刪除 "${cartProductName}"`);
-        getCartList();
-    })
-    .catch(function (error) {
-        console.log(error.response.data);
-    })
+        .then(function (res) {
+            alert(`已刪除 "${cartProductName}"`);
+            getCartList();
+        })
+        .catch(function (error) {
+            console.log(error.response.data);
+        })
 });
 
 // 刪除所有購物車內容
 const discardAllBtn = document.querySelector('.discardAllBtn');
-discardAllBtn.addEventListener('click',(e)=>{
+discardAllBtn.addEventListener('click', (e) => {
     e.preventDefault();
     axios.delete(`${api_url}/${api_path}/carts`)
-    .then(function (res) {
-        alert(`已刪除所有購物車內容`);
-        getCartList();
-    })
-    .catch(function (error) {
-        alert(`購物車中已無商品`);
-    })
+        .then(function (res) {
+            alert(`已刪除所有購物車內容`);
+            getCartList();
+        })
+        .catch(function (error) {
+            alert(`購物車中已無商品`);
+        })
 });
 // 記錄點︰90分鐘
 
 
 //送出訂單
 const orderInfoBtn = document.querySelector('.orderInfo-btn');
-orderInfoBtn.addEventListener('click',(e)=>{
+orderInfoBtn.addEventListener('click', (e) => {
     e.preventDefault();
     //1. 購物車中要有品項
-    if(cartData.length == 0){
+    if (cartData.length == 0) {
         alert('訂單送出失敗：購物車中沒有任何商品');
         return;
     };
@@ -186,10 +186,12 @@ orderInfoBtn.addEventListener('click',(e)=>{
     const customerEmail = document.querySelector('#customerEmail').value;
     const customerAddress = document.querySelector('#customerAddress').value;
     const tradeWay = document.querySelector('#tradeWay').value;
-    if(customerName=="" || customerPhone=="" || customerEmail=="" || customerAddress=="" || tradeWay=="" ){
+    if (customerName == "" || customerPhone == "" || customerEmail == "" || customerAddress == "" || tradeWay == "") {
         alert('還有資料未填寫');
         return;
     }
+
+    alertEmailError(customerEmail);
     axios.post(`${api_url}/${api_path}/orders`, {
         "data": {
             "user": {
@@ -201,18 +203,105 @@ orderInfoBtn.addEventListener('click',(e)=>{
             }
         }
     })
-    .then(function (res) {
-        alert('成功送出訂單');
-        document.querySelector('#customerName').value = "";
-        document.querySelector('#customerPhone').value = "";
-        document.querySelector('#customerEmail').value = "";
-        document.querySelector('#customerAddress').value = "";
-        document.querySelector('#tradeWay').value = "ATM";
-        getCartList();
-    })
-    .catch(function (error) {
-        console.log(error.response.data);
-    })
+        .then(function (res) {
+            alert('成功送出訂單');
+            document.querySelector('#customerName').value = "";
+            document.querySelector('#customerPhone').value = "";
+            document.querySelector('#customerEmail').value = "";
+            document.querySelector('#customerAddress').value = "";
+            document.querySelector('#tradeWay').value = "ATM";
+            getCartList();
+        })
+        .catch(function (error) {
+            console.log(error.response.data);
+        })
 });
 
 //建議前後台加總 8 小時內完成
+
+
+// 表單驗證
+const customerEmail = document.querySelector("#customerEmail");
+customerEmail.addEventListener("blur", function (e) {
+    if (validateEmail(customerEmail.value) == false) {
+        document.querySelector(`[data-message=Email]`).textContent = "請填寫正確 Email 格式";
+        return;
+    }
+})
+
+// validate.js
+const inputs = document.querySelectorAll("input[name],select[data=payment]");
+console.log(inputs);
+const form = document.querySelector(".orderInfo-form");
+const constraints = {
+    "姓名": {
+        presence: {
+            message: "必填欄位"
+        }
+    },
+    "電話": {
+        presence: {
+            message: "必填欄位"
+        },
+        length: {
+            minimum: 8,
+            message: "需超過 8 碼"
+        }
+    },
+    "信箱": {
+        presence: {
+            message: "必填欄位"
+        },
+        email: {
+            message: "格式錯誤"
+        }
+    },
+    "寄送地址": {
+        presence: {
+            message: "必填欄位"
+        }
+    },
+    "交易方式": {
+        presence: {
+            message: "必填欄位"
+        }
+    },
+};
+
+
+inputs.forEach((item) => {
+    // change, blur(移開焦點), input(填寫值改變時觸發)
+    item.addEventListener("change", function () {
+
+        item.nextElementSibling.textContent = '';
+        let errors = validate(form, constraints) || '';
+
+        if (errors) {
+            Object.keys(errors).forEach(function (keys) {
+                console.log(document.querySelector(`[data-message=${keys}]`))
+                document.querySelector(`[data-message="${keys}"]`).textContent = errors[keys];
+            })
+        }
+    });
+});
+
+// email驗證
+function validateEmail(mail) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
+        return true
+    }
+    return false;
+}
+function alertEmailError() {
+
+    if (!validateEmail(customerEmail)) {
+        alert('email 格式有誤！');
+        return;
+    }
+}
+function validatePhone(phone) {
+    if (/^[09]{2}\d{8}$/.test(phone)) {
+        return true
+    }
+    return false;
+}
