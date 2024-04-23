705539fa4d9fd1688c9d2063e0d255b672080711
Rxzan <ostrich2821@example.com>
2024-04-24 03:00:34 +0800

    更新 deploy

--- vite.config.js
+++ vite.config.js
@@ -23,9 +23,7 @@ function moveOutputPlugin() {
 }
 
 export default defineConfig({
-  // base 的寫法：
-  // base: '/Repository 的名稱/'
-  base: '/w9-homework/',
+  base: '/woworoom-practice/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
